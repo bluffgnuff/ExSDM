934cf601e3b486cdbdd2932fd73136234e078b98
stefano <stefano.romeo@studio.unibo.it>
2021-06-07 13:28:35 +0200

    minor fixes

--- ex2-Spring/src/main/java/com/DemoApplication.java
+++ ex2-Spring/src/main/java/com/DemoApplication.java
@@ -2,12 +2,8 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.stereotype.Controller;
 
 @SpringBootApplication
-/*@Controller*/
 public class DemoApplication{
 
     public static void main(String[] args) {
