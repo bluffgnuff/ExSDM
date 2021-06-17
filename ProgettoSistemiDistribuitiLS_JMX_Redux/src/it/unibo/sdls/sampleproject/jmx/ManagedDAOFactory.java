f60a6f689ae9b740fea955a5149966658de481a9
stefano <stefano.romeo@studio.unibo.it>
2021-06-17 15:35:07 +0200

    Project working

---
+++ ProgettoSistemiDistribuitiLS_JMX_Redux/src/it/unibo/sdls/sampleproject/jmx/ManagedDAOFactory.java
@@ -0,0 +1,58 @@
+package it.unibo.sdls.sampleproject.jmx;
+
+import it.unibo.sdls.sampleproject.dao.DAOFactory;
+import it.unibo.sdls.sampleproject.dao.jdbc.pooled.PooledGenericJDBCDAOFactory;
+
+import javax.servlet.ServletContext;
+
+public class ManagedDAOFactory implements ManagedDAOFactoryMBean{
+    private  PooledGenericJDBCDAOFactory pgf;
+    private String className;
+    ServletContext context;
+
+
+    /*public void setContext(ServletContext context){
+        this.context = context;
+        pgf = (PooledGenericJDBCDAOFactory) DAOFactory.getDAOFactory(context.getInitParameter("daoFactory"));
+        context.setAttribute("daoFactory", context.getInitParameter("daoFactory"));
+    }*/
+    public ManagedDAOFactory(ServletContext context){
+       this.context = context;
+       this.className = context.getInitParameter("daoFactory");
+       context.setAttribute("daoFactory", className);
+    }
+
+    public int getPoolSize() {
+        return pgf.getPoolSize();
+    }
+
+    @Override
+    public void setPoolSize(int ps) {
+        pgf.setPoolSize(ps);
+    }
+
+    @Override
+    public long getMaxWait() {
+        return pgf.getMaxWait();
+    }
+
+    @Override
+    public void setMaxWait(long mw) {
+        pgf.setMaxWait(mw);
+    }
+
+    @Override
+    public String using() {
+        return pgf.getClass().getCanonicalName();
+    }
+
+    @Override
+    public boolean switchTo(String className) {
+        if(className.equals(pgf.getClass().getName()))
+            return false;
+
+        context.setAttribute("daoFactory", className);
+        this.className=className;
+        return true;
+    }
+}
